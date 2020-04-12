4475f1cba33a045f2e12f08661203439f3af5d2e
Luan Andrade <luan10017@hotmail.com>
2020-04-12 11:57:11 -0300

    Adicionado estrutura de objeto com vetor incorporado

---
+++ 01-2-tecnologia.js
@@ -0,0 +1,16 @@
+/* Vetores e objetos
+Crie um programa com um objeto para armazenar dados de um programador como nome, idade e tecnologias que trabalha.
+
+Um programador pode trabalhar com várias tecnologias, por isso armazene essas tecnologias em um array.
+
+As tecnologias também devem ser objetos contendo nome e especialidade, use as tecnologias abaixo: */
+
+const programador = {
+    nome: "Luan",
+    idade: 26,
+    tecnologias: [
+        {nome: "Python", especialidade: "Datascience"},
+        {nome: "C", especialidade: "microcontroladores"},
+        {nome: "SQL", especialidade: "Banco de dados"}
+    ]
+};
